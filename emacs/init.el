c7116cce3ca0167568ef8b19f3893fc7a4bac288
viemacs <emacguo@gmail.com>
2016-11-24 14:45:44 +0800

    create init.el, set autosave and backup locatoin

---
+++ emacs/init.el
@@ -0,0 +1,9 @@
+;; put autosave and backup files into ~/.emacs.d/{autosave,save}
+; create the directories is necessary
+(make-directory "~/.emacs.d/autosave/" t)
+(make-directory "~/.emacs.d/save/" t)
+; specific directory for autosave and backup
+(custom-set-variables
+  '(auto-save-file-name-transforms '((".*" "~/.emacs.d/autosave/" t)) )
+  '(backup-directory-alist '((".*" . "~/.emacs.d/save")) ) )
+
