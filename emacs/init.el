90ac20f870653ed637aca964c208a67504629aaf
viemacs <emacguo@gmail.com>
2016-11-24 15:06:58 +0800

    remove welcome message; set a color theme

--- emacs/init.el
+++ emacs/init.el
@@ -4,8 +4,23 @@
 (make-directory "~/.emacs.d/save/" t)
 ; specific directory for autosave and backup
 (custom-set-variables
-  '(auto-save-file-name-transforms '((".*" "~/.emacs.d/autosave/" t)) )
-  '(backup-directory-alist '((".*" . "~/.emacs.d/save")) ) )
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(auto-save-file-name-transforms (quote ((".*" "~/.emacs.d/autosave/" t))))
+ '(backup-directory-alist (quote ((".*" . "~/.emacs.d/save"))))
+ '(custom-enabled-themes (quote (wombat))))
 
+;; remove the welcome message
+(setq inhibit-startup-message t)
 ;; disable the menu bar
-(menu-bar-mode -1)
+;(menu-bar-mode -1)
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
