6cef38faf7e6f52f14b02757fa0e22a4c8d63225
viemacs <emacguo@gmail.com>
2016-11-30 10:25:32 +0800

    update huffman.py

--- alg/huffman.py
+++ alg/huffman.py
@@ -1,40 +1,35 @@
 #!/usr/bin/python
 
-def getFrequency(counts):
-    total = sum(counts)
-    return list(map(lambda x:x/total, counts))
-
 class Node(object):
     def __init__(self, symbol = '', weight = 0, left = None, right = None):
-        self.symbol = symbol
-        self.left = left
-        self.right = right
-        self.weight = weight
+        self.symbol, self.weight, self.left, self.right = symbol, weight, left, right
         if (left and right):
             self.weight = left.weight + right.weight
 
     def result(self, path = ''):
-        if self.symbol:
-            print(self.symbol, path)
-        if self.left:
-            self.left.result(path + '0')
-        if self.right:
-            self.right.result(path + '1')
+        self.symbol and print(self.symbol, path)
+        self.left and self.left.result(path + '0')
+        self.right and self.right.result(path + '1')
+
 
 def genHuffman(symbol, counts):
     nodeList = []
     for i in range(len(symbol)):
         nodeList.append(Node(symbol[i], counts[i]))
     
-    
     while (len(nodeList) >= 2):
         nodeList.sort(key = lambda x: x.weight, reverse = True)
         nodeList.append(Node(right = nodeList.pop(), left = nodeList.pop()))
-    
-    nodeList[0].result()
+
+    return nodeList
+
+
+def getFrequency(counts):
+    total = sum(counts)
+    return list(map(lambda x:x/total, counts))
 
 # test
 symbol = ['A', 'B', 'C', 'D', 'E']
 counts = [15, 7, 6, 6, 5]
 
-genHuffman(symbol, counts)
+genHuffman(symbol, counts)[0].result()
